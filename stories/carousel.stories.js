6d2e661800a040d4ace92aefcbe5f0810f35524a
francisco jimnenez <frankiiize@gmail.com>
2021-01-02 12:39:33 -0300

    platzi video ejemplo

---
+++ stories/carousel.stories.js
@@ -0,0 +1,5 @@
+export default { title: "carousel" };
+import carousel from "../components/carousel/carousel.html";
+import "../components/carousel/carousel.css"; 
+  
+  export const withImages = () => carousel;
---
+++ stories/input.stories.js
@@ -0,0 +1,5 @@
+export default { title: "input" };
+import input from "../components/imput/input.html";
+import "../components/imput/inputStyles.css"; 
+  
+  export const withImages = () => input;
